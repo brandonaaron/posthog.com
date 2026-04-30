5bf3bad22c1a61c00b553c2cc8b8de39243f2636
Michael Matloka <dev@twixes.com>
2026-04-30 12:18:31 +0100

    Remove children from Inbox navigation item
    Removed children property from Inbox navigation item.

--- src/navs/index.js
+++ src/navs/index.js
@@ -6413,7 +6413,6 @@ export const docsMenu = {
                     url: '/docs/posthog-code/inbox',
                     icon: 'IconInbox',
                     color: 'yellow',
-                    children: [{ name: 'Overview', url: '/docs/posthog-code/inbox' }],
                 },
                 {
                     name: 'Skills',
